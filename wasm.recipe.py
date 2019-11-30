68257dd573550a721edcf24344df64324ae65d25
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-11-30 12:08:23 +0100

    Setup wasm build

--- wasm.recipe.py
+++ wasm.recipe.py
@@ -2,10 +2,6 @@ from conans import ConanFile, CMake
 
 class OpenGlPlaygroundWasm(ConanFile):
     settings = {"os": ["Emscripten"]}
-    requires = ["entt/3.1.1@skypjack/stable",
-      "glm/0.9.8.5@bincrafters/stable", 
-      "spdlog/1.3.1@bincrafters/stable",
-      "imgui/1.69@bincrafters/stable"]
     generators = ["cmake", "txt"]
 
     def _configure_cmake(self):
