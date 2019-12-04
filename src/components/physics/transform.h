63bf2f14c7ebdb709241419af238869d4943510b
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-12-04 14:20:52 +0100

    Add unit test support

---
+++ src/components/physics/transform.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace comp {
+	/**
+	 * @brief Store position, rotation and scaling of an entity in the world
+	 */
+	struct Transform {
+		glm::vec3 position;
+		float scale;
+	};
+}
---
+++ test/maths/map-generation.test.cpp
@@ -0,0 +1,15 @@
+#include <catch2/catch.hpp>
+
+#include "maths/map-generation.h"
+
+SCENARIO("Explain", "[map-generation]") {
+    GIVEN("Blabla") {
+
+        WHEN("blabla") {
+
+            THEN("blabla") {
+                REQUIRE(true);
+            }
+        }
+    }
+}
