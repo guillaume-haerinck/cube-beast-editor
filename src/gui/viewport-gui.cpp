dfd6d31eec95cb9ee5c63a1a7e634d59902f3d55
Guillaume <guillaume.haerinck@outlook.com>
2019-12-21 00:02:52 +0100

    Create a dockspace

--- src/gui/viewport-gui.cpp
+++ src/gui/viewport-gui.cpp
@@ -18,13 +18,42 @@ ViewportGui::ViewportGui(Context& ctx) : m_ctx(ctx) {
 ViewportGui::~ViewportGui() {}
 
 void ViewportGui::update() {
-    ImGui::ShowDemoWindow();
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->Pos);
+    ImGui::SetNextWindowSize(viewport->Size);
+    ImGui::SetNextWindowViewport(viewport->ID);
+    
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    window_flags |= ImGuiWindowFlags_NoBackground;
+    
+    bool open = true;
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 
-    ImGui::Begin("Main debug window");
-        ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    ImGui::End();
+    ImGui::Begin("DockSpace Demo", &open, window_flags);
+        ImGui::PopStyleVar(3);
 
-    ImGui::Begin("Other window");
-        ImGui::Text("hello");
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f));
+
+        if(ImGui::BeginMenuBar()){
+            if (ImGui::BeginMenu("Docking")) {
+                ImGui::MenuItem("Flag: NoSplit");
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+
+        ImGui::Begin("Main debug window");
+            ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+
+        ImGui::Begin("Other window");
+            ImGui::Text("hello");
+        ImGui::End();
+
     ImGui::End();
 }
